3976f4f4b9602643c27faf32511e129f5138581f
waterlzj <waterlzj@gmail.com>
2018-09-03 18:54:26 +0800

    9

---
+++ 9_palindrome_number.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x < 0)
+            return false;
+        if(x < 10)
+            return true;
+        if(x % 10 == 0)
+            return false;
+        int y = 0;
+        while(x > y)
+        {
+            int tmp = x % 10;
+            x = x / 10;
+            if(x == y)
+                return true;
+            
+            y *= 10;
+            y += tmp;
+            
+            if(x == y)
+                return true;
+        }
+        return false;
+    }
+};
